a2f7e5886dec1d0b1574568316a478fb123dbbcc
Rafael-Friedel <RafaFriedelDev@outlook.com>
2022-02-18 15:32:42 -0300

    REQUISITO 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <main>
+        <Header />
+      </main>
+    );
   }
 }
 
 export default App;
 
-// VQV
+// VQV;
