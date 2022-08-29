71c6acf9af0f42601f5a87db6f3017ceefda8a7e
Rafael <RafaFriedelDev@outlook.com>
2022-08-29 19:46:01 -0300

    feat: subindo project para o github

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Header from './components/Header';
-import SolarSystem from './components/SolarSystem';
-import Missions from './components/Missions';
 import './App.css';
+import Header from './components/Header';
+import Missions from './components/Missions';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
@@ -11,12 +11,9 @@ class App extends React.Component {
         <Header />
         <SolarSystem />
         <Missions />
-        <p>CREDITO: DEBS</p>
       </main>
     );
   }
 }
 
 export default App;
-
-// VQV;
